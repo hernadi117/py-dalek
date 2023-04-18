42d4803e21ac86c14745336776811d4a921663e4
Victor <vhernadi@kth.se>
2023-04-18 03:17:12 +0200

    some more docstrings

--- components.py
+++ components.py
@@ -4,26 +4,55 @@ import pygame as pg
 
 @component
 class Position:
+    """
+    Position component.
+    Args:
+    x: x-coordinate
+    y: y-coordinate
+    """
     x: int
     y: int
 
 @component
 class Velocity:
+    """
+    Velocity component.
+    Args:
+    x: x-velocity component
+    y: y-velocity component
+    """
     x: int
     y: int
 
 
 @component
 class AI:
+    """
+    AI component marker.
+    """
     pass
 
 
 @component
 class Renderable:
+    """
+    Renderable component.
+    Args:
+    sprite: the sprite to be rendered
+    """
     sprite: pg.Surface
 
 @component
 class Animation:
+    """
+    Animation component.
+    Args:
+    sheet: the sprite-sheet to be used for animation
+    frame_dt: the delta time to pass between animation frames
+    curr: the current sprite selected for animation
+    elapsed: the elapsed time
+    once: False if this should be removed after one complete animation, True otherwise
+    """
     sheet: list[pg.Surface]
     frame_dt: int
     curr: int = 0
@@ -33,4 +62,7 @@ class Animation:
 
 @component
 class Player:
+    """
+    Player component marker.
+    """
     pass
